9c7b0e709dd3b51d834d6edae0000e522e0887e8
Xabura <73250262+Xabura@users.noreply.github.com>
2021-10-31 19:06:42 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function toggle(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active')
+    var burger = document.getElementById('burger');
+    burger.classList.toggle('active')
+    var close = document.getElementById('close');
+    close.classList.toggle('active')
+}
